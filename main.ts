14bfc1e43c44ddc49694feb66459409b5f06ea1b
Josué Martínez <30963079+monkmonkeykey@users.noreply.github.com>
2022-05-10 23:33:33 -0500

    Carrito con sensores

--- main.ts
+++ main.ts
@@ -1,33 +1,75 @@
-input.onButtonPressed(Button.A, function () {
-    radio.sendValue("motores", 1)
-})
-input.onButtonPressed(Button.AB, function () {
-    radio.sendValue("motores", 0)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendValue("motores", 2)
-})
-radio.onReceivedValue(function (name, value) {
-    if (name == "motores" && value == 1) {
+/**
+ * Adelante
+ */
+/**
+ * Izquierda
+ */
+/**
+ * Derecha
+ */
+/**
+ * Stop
+ */
+function runWithsensores () {
+    if (pins.digitalReadPin(DigitalPin.P5) == 1 && pins.digitalReadPin(DigitalPin.P6) == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            # # # # #
+            . . . . .
+            `)
         pins.digitalWritePin(DigitalPin.P0, 1)
         pins.digitalWritePin(DigitalPin.P1, 0)
         pins.digitalWritePin(DigitalPin.P2, 1)
         pins.digitalWritePin(DigitalPin.P3, 0)
-    } else if (name == "motores" && value == 2) {
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        pins.digitalWritePin(DigitalPin.P3, 1)
-    } else if (name == "motores" && value == 0) {
-        pins.digitalWritePin(DigitalPin.P0, 0)
+    } else if (pins.digitalReadPin(DigitalPin.P5) == 1 && pins.digitalReadPin(DigitalPin.P6) == 0) {
+        basic.showLeds(`
+            . . . # .
+            . . # # .
+            . # . # .
+            . . # # .
+            . . . # .
+            `)
+        pins.digitalWritePin(DigitalPin.P0, 1)
         pins.digitalWritePin(DigitalPin.P1, 0)
         pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P3, 1)
+    } else if (pins.digitalReadPin(DigitalPin.P5) == 0 && pins.digitalReadPin(DigitalPin.P6) == 1) {
+        basic.showLeds(`
+            . # . . .
+            . # # . .
+            . # . # .
+            . # # . .
+            . # . . .
+            `)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+    } else if (pins.digitalReadPin(DigitalPin.P5) == 0 && pins.digitalReadPin(DigitalPin.P6) == 0) {
+        basic.showLeds(`
+            . # # # .
+            # . . # #
+            # . # . #
+            # # . . #
+            . # # # .
+            `)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P3, 0)
         pins.digitalWritePin(DigitalPin.P3, 0)
     } else {
     	
     }
-})
-radio.setGroup(1)
+}
+let control2 = 0
 basic.forever(function () {
-	
+    if (input.runningTime() / 1000 >= 5) {
+        runWithsensores()
+    } else if (input.runningTime() / 1000 <= 4) {
+        basic.showNumber(input.runningTime() / 1000)
+    } else {
+    	
+    }
 })
